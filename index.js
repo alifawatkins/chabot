162dc1bbe421124e80910d0af0ab169953c17d0d
littlelionstudios <flavorformulas@gmail.com>
2023-02-01 13:48:41 -0500

    Testing

---
+++ index.js
@@ -0,0 +1,11 @@
+// Ask user for input
+var userName = prompt("Hello, what is your name?");
+
+var mood = prompt("How are you "  + userName + "?");
+
+// alert the user
+alert(userName + ", how you feel right now is not important.");
+
+var reason = prompt("Why are you here, " + userName + "?");
+
+alert("Not good enough. Go back to sleep, " + userName);
